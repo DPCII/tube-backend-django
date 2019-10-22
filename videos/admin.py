e2273fe93a1ee622692796f2d8da9cd8e8b78852
D Paul Childs <clausewitzian@gmail.com>
2019-10-21 22:24:03 -0400

    created sciences

--- videos/admin.py
+++ videos/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Subject, Time_Period, Video_Entry, Region
+from .models import Subject, Time_Period, Video_Entry, Region, Natural_Science, Formal_Science, Natural_Science_Video
 
 # Register your models here.
 
 admin.site.register(Subject)
 admin.site.register(Time_Period)
 admin.site.register(Video_Entry)
-admin.site.register(Region)
+admin.site.register(Natural_Science)
+admin.site.register(Formal_Science)
+admin.site.register(Natural_Science_Video)
+
+
--- videos/models.py
+++ videos/models.py
@@ -13,7 +13,6 @@ class Subject(models.Model):
     topic = models.CharField(
         max_length=50,
         choices=SUBJECTS_CHOICES,
-        default=HISTORY,
     )
 
     def __str__(self):
@@ -83,6 +82,70 @@ class Video_Entry(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="video_entry")
     region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name="video_entry")
     title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+
+class Formal_Science(models.Model):
+    MATHEMATICS = 'mathematics'
+    COMPUTER_SCIENCE = 'computer science'
+    DISCIPLINE_CHOICES = [
+        (MATHEMATICS, 'mathematics'),
+        (COMPUTER_SCIENCE, 'computer science'),
+    ]
+    formal_science = models.CharField(
+        choices=DISCIPLINE_CHOICES,
+        max_length=100
+        )
+
+    def __str__(self):
+        return self.formal_science
+
+
+class Natural_Science(models.Model):
+    CHEMISTRY = 'chemistry'
+    BIOLOGY = 'biology'
+    ZOOLOGY = 'zoology'
+    PHYSICS = 'physics'
+    ASTRONOMY = 'astronomy'
+    DISCIPLINE_CHOICES = [
+        (CHEMISTRY, 'chemistry'),
+        (BIOLOGY, 'biology'),
+        (ZOOLOGY, 'zoology'),
+        (PHYSICS, 'physics'),
+        (ASTRONOMY, 'astronomy'),
+    ]
+    natural_science = models.CharField(
+        choices=DISCIPLINE_CHOICES,
+        max_length=100
+    )
+
+    def __str__(self):
+        return self.natural_science
+
+
+class Natural_Science_Video(models.Model):
+    YOUTUBE = "youtube"
+    VIMEO = "vimeo"
+    NATIONAL_ARCHIVES = "national archives"
+    PLAYER_CHOICES = [
+        (YOUTUBE, 'youtube'),
+        (VIMEO, 'vimeo'),
+        (NATIONAL_ARCHIVES, 'national archives'),
+    ]
+    vid_url = models.URLField('Web Address')
+    resId = models.CharField(max_length=255)
+    vid_player = models.CharField(
+        max_length=40,
+        choices=PLAYER_CHOICES,
+        default=YOUTUBE,
+    )
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="science_video")
+    natural_science = models.ForeignKey(Natural_Science, on_delete=models.CASCADE, related_name="science_video")
+    tags = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
