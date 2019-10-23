e186719231185aebe567b1a1e60a7db8215a016c
D Paul Childs <clausewitzian@gmail.com>
2019-10-23 11:00:47 -0400

    added production setting to github

---
+++ video_backend/settings/production.py
@@ -0,0 +1,32 @@
+from video_backend.settings.common import *
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_value(env_variable):
+    try:
+        return os.environ[env_variable]
+    except KeyError:
+        error_msg = 'Set the {} environment variable'.format(var_name)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_env_value('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_value('DB_NAME'),
+        'USER': get_env_value('DB_USER'),
+        'PASSWORD': get_env_value('DB_PASSWORD'),
+        'HOST': get_env_value('DB_HOST'),
+        'PORT': get_env_value('DB_PORT'),
+    }
+}
